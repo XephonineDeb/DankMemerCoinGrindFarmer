c4df4648552c7f072967d2ff8a2d9ee190be8d8a
Xephonine <88765049+XephonineDeb@users.noreply.github.com>
2021-12-22 19:49:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+while True:
+	pyautogui.write('pls beg', interval=0.1)  # type with quarter-second pause in between each key
+	pyautogui.press('enter')   
+	pyautogui.write('pls hunt', interval=0.1)  # type with quarter-second pause in between each key
+	pyautogui.press('enter')   
+	pyautogui.write('pls fish', interval=0.1)  # type with quarter-second pause in between each key
+	pyautogui.press('enter')   
+	pyautogui.write('pls beg', interval=0.1)  # type with quarter-second pause in between each key
+	pyautogui.press('enter')   
+	pyautogui.write('pls kill @Dank', interval=0.1)  # type with quarter-second pause in between each key
+	pyautogui.press('enter')
+	pyautogui.press('enter')
+	pyautogui.write("pls search")
+	pyautogui.press('enter')
+	time.sleep(2)
+	pyautogui.click(515,746)
+	time.sleep(2)   
